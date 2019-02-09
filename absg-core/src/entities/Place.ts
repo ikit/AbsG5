e6260588420dd7448d38c1d9b2f30e2d9acb122f
Olivier Gueudelot <olivier@labsquare.org>
2019-02-09 18:14:29 +0100

    fake data for dev and test

--- absg-core/src/entities/Place.ts
+++ absg-core/src/entities/Place.ts
@@ -7,11 +7,14 @@ export class Place {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({ comment: 'Nom de l\'endroit ' })
+    @Column({ comment: 'Nom de l\'endroit' })
     name: string;
 
     @Column({ nullable: true, comment: 'Description' })
     description: string;
+
+    @Column({ comment: 'Coordonnée GPS' })
+    location: string;
 
     @OneToMany(type => Person, p => p.id)
     inhabitants: Person[];
