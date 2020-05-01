10f5de64e94973452eedc0d66b03597f79d460ea
Olivier Gueudelot <olivier@labsquare.org>
2020-05-01 17:31:26 +0200

    wip: test agpa ceremony

---
+++ absg-client/src/components/Timer.vue
@@ -0,0 +1,131 @@
+<template>
+<div style="text-align: center">
+    <ul class="vuejs-countdown">
+        <li v-if="days > 0">
+            <p class="digit">{{ days | twoDigits }}</p>
+            <p class="text">{{ days > 1 ? 'jours' : 'jour' }}</p>
+        </li>
+        <li>
+            <p class="digit">{{ hours | twoDigits }}</p>
+            <p class="text">{{ hours > 1 ? 'heures' : 'heur' }}</p>
+        </li>
+        <li>
+            <p class="digit">{{ minutes | twoDigits }}</p>
+            <p class="text">min</p>
+        </li>
+        <li>
+            <p class="digit">{{ seconds | twoDigits }}</p>
+            <p class="text">sec</p>
+        </li>
+    </ul>
+</div>
+</template>
+
+<script>
+let interval = null;
+export default {
+    props: {
+        end: {
+            type: Date
+        }
+    },
+    data: () => ({
+        now: Math.trunc((new Date()).getTime() / 1000),
+        date: null,
+        diff: 0
+    }),
+    created() {
+        this.init();
+    },
+    computed: {
+        seconds() {
+            return Math.trunc(this.diff) % 60
+        },
+        minutes() {
+            return Math.trunc(this.diff / 60) % 60
+        },
+        hours() {
+            return Math.trunc(this.diff / 60 / 60) % 24
+        },
+        days() {
+            return Math.trunc(this.diff / 60 / 60 / 24)
+        }
+    },
+    watch: {
+        now(value) {
+            this.diff = this.date - this.now;
+            if(this.diff <= 0){
+                this.diff = 0;
+                clearInterval(interval);
+                this.$emit("completed")
+            }
+        }
+    },
+    filters: {
+        twoDigits(value) {
+            if ( value.toString().length <= 1 ) {
+                return '0'+value.toString()
+            }
+            return value.toString()
+        }
+    },
+    destroyed() {
+        clearInterval(interval);
+    },
+    methods: {
+        init(endDate=null) {
+            if (this.end) {
+                this.date = Math.trunc(this.end.getTime() / 1000);
+                if (!this.date) {
+                    throw new Error("Date invalide");
+                }
+                interval = setInterval(() => {
+                    this.now = Math.trunc((new Date()).getTime() / 1000);
+                }, 1000);
+            }
+        }
+    }
+}
+</script>
+<style>
+.vuejs-countdown {
+    padding: 0;
+    margin: 0;
+}
+.vuejs-countdown li {
+    display: inline-block;
+    margin: 0 8px;
+    text-align: center;
+    position: relative;
+}
+.vuejs-countdown li p {
+    margin: 0;
+}
+.vuejs-countdown li:after {
+    content: ":";
+    position: absolute;
+    top: 0;
+    right: -13px;
+    font-size: 32px;
+}
+.vuejs-countdown li:first-of-type {
+    margin-left: 0;
+}
+.vuejs-countdown li:last-of-type {
+    margin-right: 0;
+}
+.vuejs-countdown li:last-of-type:after {
+    content: "";
+}
+.vuejs-countdown .digit {
+    font-size: 32px;
+    font-weight: 600;
+    line-height: 1.4;
+    margin-bottom: 0;
+}
+.vuejs-countdown .text {
+    text-transform: uppercase;
+    margin-bottom: 0;
+    font-size: 10px;
+}
+</style>
--- absg-client/src/views/Agpa/CeremonyMenu.vue
+++ absg-client/src/views/Agpa/CeremonyMenu.vue
@@ -5,6 +5,18 @@
         2019
     </v-btn>
 
+    <h1>12<sup>ème</sup> cérémony des A.G.P.A.</h1>
+    <p style="text-align:center"> Ouverture dans </p>
+    <Timer ref="timer" style="margin: auto"  v-on:completed="startCeremony()"></Timer>
+
+
+
+    <div v-bind:class="{ ceremony: current.displayed }">
+        <video ref="video" style="margin: auto;" width="100%" height="100%" controls>
+            <source src="http://dev.absolumentg.fr/files/agpa/intro.mp4" type="video/mp4" >
+            Your browser does not support the video tag.
+        </video>
+    </div>
 </div>
 </template>
 
@@ -13,20 +25,81 @@
 import axios from 'axios';
 import { agpaPhotoToGalleryPhoto } from '../../middleware/AgpaHelper';
 import { padNumber } from '../../middleware/CommonHelper';
+import Timer from '../../components/Timer';
 
 export default {
     name: 'CeremonyMenu',
+    components: {
+        Timer,
+    },
     data: () => ({
         isLoading: false,
-        year: 2018,
+        year: 2020,
+        current: {
+            year: 2020,
+            ceremonyDate: null,
+            displayed: false
+        }
+
     }),
     mounted() {
+        // TODO: récupérer la date de la cérémonie depuis le back et initialiser le compte à rebours
+        this.current.ceremonyDate = new Date( new Date().getTime() + 10000000);
+        console.log( this.current.ceremonyDate);
     },
+    methods: {
+        startCeremony() {
+            console.log("TIMER DONE !");
+            this.current.displayed = true;
+
+            console.log(this.$refs.video);
+            this.$refs.video.play();
+        }
+    }
 };
 </script>
 
 <style lang="scss" scoped>
 @import '../../themes/global.scss';
+
+
+
+.ceremony {
+    opacity:0;
+    position: fixed;
+    overflow: hidden;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: #000;
+    z-index: 10000;
+    -moz-animation: anim 3s linear forwards;
+    -webkit-animation: anim 3s linear forwards;
+    -o-animation: anim 3s linear forwards;
+    -ms-animation: anim 3s linear forwards;
+    animation: anim 3s linear forwards;
+}
+@-moz-keyframes anim {
+   0%  {opacity:0;}
+   100% {opacity:1;}
+}
+@-webkit-keyframes anim {
+   0%  {opacity:0;}
+   100% {opacity:1;}
+}
+@-o-keyframes anim {
+   0%  {opacity:0;}
+   100% {opacity:1;}
+}
+@-ms-keyframes anim {
+   0%  {opacity:0;}
+   100% {opacity:1;}
+}
+@keyframes anim {
+   0%  {opacity:0;}
+   100% {opacity:1;}
+}
 
 
 </style>
