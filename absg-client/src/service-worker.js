1bd99eeead840c1e254333c1585b2d673fd5bf3c
Olivier Gueudelot <olivier@labsquare.org>
2020-11-01 12:02:36 +0100

    test service worker update

--- absg-client/src/service-worker.js
+++ absg-client/src/service-worker.js
@@ -4,7 +4,7 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 // install new service worker when ok, then reload page.
 self.addEventListener("message", msg=>{
-    if (msg.data.action=='skipWaiting'){
+    if (msg.data.action == "skipWaiting"){
         self.skipWaiting()
     }
 })
